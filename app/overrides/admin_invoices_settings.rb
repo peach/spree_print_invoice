98d3fb573111490bd7f51a287d3a873d5908add0
Miguel Sancho Fernandez <sanchocuba@gmail.com>
2016-01-21 17:50:12 -0500

    fixed selector for deface

--- app/overrides/admin_invoices_settings.rb
+++ app/overrides/admin_invoices_settings.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/admin/general_settings/edit",
                      :name => "admin_print_invoice_settings",
-                     :insert_bottom => ".omega.six.columns",
+                     :insert_bottom => ".alpha.six.columns",
                      :partial => "spree/admin/general_settings/invoice",
                      # :disabled => false)
                      :disabled => !Object.const_defined?('SpreePrintInvoice'))
